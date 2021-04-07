37a88b952cbf0ad8f577fd9af7f8cec402c295a9
hasanosman2020 <hasan79.hoa@gmail.com>
2021-04-07 13:36:17 +0100

    installed html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
-    devtool: 'source-map',
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.m?js$/,
                 exclude: /node_modules/,
-                loader: "babel-loader"
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
             }
+        ],
+        plugins: [
+            new HtmlWebPackPlugin({
+                template: "./src/client/views/index.html",
+                filename: "./index.html"
+            })
         ]
     }
 }
